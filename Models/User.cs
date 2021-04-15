af2765fe6faa7eeb41e49b0b606adc491320ad5d
lb-beep <77352348+lb-beep@users.noreply.github.com>
2021-04-15 10:22:58 -0600

    Add files via upload

---
+++ Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace MyGram.Models
+{
+    public class User
+    {
+        public User()
+        {
+        }
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string UserEmail { get; set; }
+
+    }
+}
